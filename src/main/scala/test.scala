6867cdcebad5fec4086a185b29da793f013376ab
Bamdad <aghilide@ualberta.ca>
2020-04-30 19:01:29 -0400

    Haha, lists are quiet flexible in Scala

--- src/main/scala/test.scala
+++ src/main/scala/test.scala
@@ -4,6 +4,11 @@ object test{
   =
   {
     var letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var myList = List("a",2,true)
+    var a= 3
+    a = 4
+    for (i <- myList)
+      println(i)
     for (i <- 1 to 10)
       println(letter(i));
   }
